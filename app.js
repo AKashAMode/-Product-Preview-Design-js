928ac56ed535c81c61d190cc4512d26f51c56482
AKashAMode <akashamode80@gmail.com>
2024-10-21 23:20:16 +0530

    commit changes

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
 
 
 function changeimgOpe(changeimg){
+    const product = document.getElementById('product-image');
 
     if('img1' == changeimg){
-
-    let product = document.getElementById('product-image');
     product.src = './images/macbook.jpg';
-
     }else if('img2' == changeimg){
-
-        let product = document.getElementById('product-image');
         product.src = './images/macbook-img-1.jpg';
     }else if('img3' == changeimg){
-
-        let product = document.getElementById('product-image');
         product.src = './images/macbook-img-2.jpg';
     }else if('img4' == changeimg){
-
-        let product = document.getElementById('product-image');
         product.src = './images/macbook-img-3.jpg';
     }else if('img5' == changeimg){
-
-        let product = document.getElementById('product-image');
         product.src = './images/macbook-img-4.jpg';
     }else if('img6' == changeimg){
-
-        let product = document.getElementById('product-image');
         product.src = './images/macbook-img-5.jpg';
     }
 
