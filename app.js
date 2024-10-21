8e8aec3bd7f74bb8dec0b2b1820233bc9e0444ba
AKashAMode <akashamode80@gmail.com>
2024-10-21 23:33:05 +0530

    bg color added

--- app.js
+++ app.js
@@ -2,19 +2,6 @@
 
 function changeimgOpe(changeimg){
     const product = document.getElementById('product-image');
-
-    if('img1' == changeimg){
-    product.src = './images/macbook.jpg';
-    }else if('img2' == changeimg){
-        product.src = './images/macbook-img-1.jpg';
-    }else if('img3' == changeimg){
-        product.src = './images/macbook-img-2.jpg';
-    }else if('img4' == changeimg){
-        product.src = './images/macbook-img-3.jpg';
-    }else if('img5' == changeimg){
-        product.src = './images/macbook-img-4.jpg';
-    }else if('img6' == changeimg){
-        product.src = './images/macbook-img-5.jpg';
-    }
+    product.src = changeimg;
 
 }
